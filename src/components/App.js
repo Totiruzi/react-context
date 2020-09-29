31edacc728450c5efe478166c4fe885d3071ba61
Totiruzi <totiruzi@yahoo.com>
2020-09-29 14:40:16 +0300

    Replace redux with contex

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,20 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSeletor';
 
 class App extends React.Component {
-  state = { language: 'english' };
-  onLanguageChange = (language) => {
-    this.setState({ language });
-  };
+  
   render() {
     return (
       <div className="ui container">
-        <div>
-          Select a language:
-          <i
-            className="flag us"
-            onClick={() => this.onLanguageChange('english')}
-          ></i>
-          <i
-            className="flag ng"
-            onClick={() => this.onLanguageChange('yoruba')}
-          ></i>
-        </div>
-        <ColorContext.Provider value="green">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector />
+          <ColorContext.Provider value="green">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
--- src/components/Field.js
+++ src/components/Field.js
@@ -4,7 +4,7 @@ import LanguageContext from '../contexts/LanguageContext';
 class Field extends React.Component {
   static contextType = LanguageContext;
   render() {
-    const text = this.context === 'english'? 'Name' : 'Oruko'
+    const text = this.context.language === 'english'? 'Name' : 'Oruko'
     return (
       <div className="ui field">
         <label>{text}</label>
