e8e80333d081fca12c19ca244241b6036b674ce0
Totiruzi <totiruzi@yahoo.com>
2020-09-29 12:34:33 +0300

    Context system in react

---
+++ src/components/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class App extends React.Component {
+  state = { language: 'english' };
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+  render() {
+    return (
+      <div className="ui container">
+        <div>
+          Select a language:
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          ></i>
+          <i
+            className="flag ng"
+            onClick={() => this.onLanguageChange('yoruba')}
+          ></i>
+        </div>
+        <ColorContext.Provider value="green">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
+      </div>
+    );
+  }
+}
+export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
+
+class Button extends React.Component {
+  renderButton = (color) => {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => (value === 'english' ? 'Submit' : 'Tẹriba')}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  };
+  render() {
+    return (
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
+    );
+  }
+}
+export default Button;
